81fc29ee6b5a9d0eae8af2a59475bc6ba6529fe2
beomjpark <beomjpark@github.com>
2021-11-29 16:50:44 +0900

    :hammer: Fix json typo in jwtInfo structure.

--- rest_server/controllers/context/context_jwt.go
+++ rest_server/controllers/context/context_jwt.go
@@ -6,7 +6,7 @@ type JwtInfo struct {
 	AccessToken  string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
 	AccessUuid   string `json:"access_uuid"`
-	RefreshUuid  string `json:"refresh_uuit"`
+	RefreshUuid  string `json:"refresh_uuid"`
 	AtExpireDt   int64  `json:"access_token_expire_dt"`
 	RtExpireDt   int64  `json:"refresh_token_expire_dt"`
 }
