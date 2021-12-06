77481db4637e6f8e9c052ab702b13a3b0f939168
beomjpark <beomjpark@github.com>
2021-12-06 12:11:56 +0900

    :fire Remove unused file "sign.go"

--- rest_server/token/sign.go
+++
@@ -1,7 +0,0 @@
-package token
-
-import "github.com/ONBUFF-IP-TOKEN/baseEthereum/ethcontroller"
-
-func VerifySign(walletAddr, msg, signHex string) bool {
-	return ethcontroller.VerifySig(walletAddr, msg, signHex)
-}
