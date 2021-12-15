b9a2c3d8757e7e402af5851a7edbe90ab9ba7d0d
beomjpark <beomjpark@github.com>
2021-12-15 21:35:56 +0900

    :hammer: Fix makeCacheKeyByAuth

--- rest_server/model/cache_jwt.go
+++ rest_server/model/cache_jwt.go
@@ -15,13 +15,13 @@ func (o *DB) SetJwtInfo(tokenInfo *context.JwtInfo, payload *context.Payload) er
 	}
 	conf := config.GetInstance()
 
-	cKey := makeCacheKeyByAuth(payload, tokenInfo.AccessUuid)
+	cKey := makeCacheKeyByAuth(payload.LoginType, tokenInfo.AccessUuid)
 	err := o.Cache.Set(cKey, tokenInfo, time.Duration(conf.Auth.AccessTokenExpiryPeriod*int64(time.Minute)))
 	if err != nil {
 		return err
 	}
 
-	cKey = makeCacheKeyByAuth(payload, tokenInfo.RefreshUuid)
+	cKey = makeCacheKeyByAuth(payload.LoginType, tokenInfo.RefreshUuid)
 	err = o.Cache.Set(cKey, tokenInfo, time.Duration(conf.Auth.RefreshTokenExpiryPeriod*int64(time.Minute)))
 	if err != nil {
 		return err
@@ -31,18 +31,18 @@ func (o *DB) SetJwtInfo(tokenInfo *context.JwtInfo, payload *context.Payload) er
 }
 
 func (o *DB) GetJwtInfo(payload *context.Payload) (*context.JwtInfo, error) {
-	cKey := makeCacheKeyByAuth(payload, payload.Uuid)
+	cKey := makeCacheKeyByAuth(payload.LoginType, payload.Uuid)
 	jwtInfo := new(context.JwtInfo)
 	err := o.Cache.Get(cKey, jwtInfo)
 	return jwtInfo, err
 }
 
 func (o *DB) DeleteJwtInfo(payload *context.Payload) error {
-	cKey := makeCacheKeyByAuth(payload, payload.Uuid)
+	cKey := makeCacheKeyByAuth(payload.LoginType, payload.Uuid)
 	err := o.Cache.Del(cKey)
 	return err
 }
 
-func makeCacheKeyByAuth(payload *context.Payload, uuid string) string {
-	return config.GetInstance().DBPrefix + ":INNO-AUTH-" + context.LoginTypeText[payload.LoginType] + ":" + uuid
+func makeCacheKeyByAuth(loginType context.LoginType, uuid string) string {
+	return config.GetInstance().DBPrefix + ":INNO-AUTH-" + context.LoginTypeText[loginType] + ":" + uuid
 }
