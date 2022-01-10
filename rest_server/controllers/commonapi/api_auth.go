93ecc10455cf4f25622d2e41c28c3a18cd8d5abe
beomjpark <beomjpark@github.com>
2022-01-10 17:11:30 +0900

    :heavy_plus_sign: Add logic to clear uuid in Redis to access token reissuance api and detect expired access token error.

--- rest_server/controllers/commonapi/api_auth.go
+++ rest_server/controllers/commonapi/api_auth.go
@@ -15,7 +15,12 @@ func GetTokenVerify(c echo.Context) error {
 	resp.Success()
 
 	ctx := base.GetContext(c).(*context.InnoAuthContext)
-	resp.Value = ctx.Payload
+
+	if _, err := auth.GetIAuth().GetJwtInfo(ctx.Payload.LoginType, ctx.Payload.Uuid); err != nil {
+		resp.SetReturn(resultcode.Result_Auth_ExpiredJwt)
+	} else {
+		resp.Value = ctx.Payload
+	}
 
 	return c.JSON(http.StatusOK, resp)
 }
@@ -27,15 +32,21 @@ func PostTokenRenew(c echo.Context, refreshTokenRequest *context.RenewTokenReque
 	if payload, err := auth.GetIAuth().VerifyRefreshToken(refreshTokenRequest.RefreshToken); err != nil {
 		resp.SetReturn(resultcode.Result_Auth_InvalidJwt)
 	} else {
-		// Make Renew Token.
-		if jwtInfoValue, err := auth.GetIAuth().MakeToken(payload); err != nil {
-			resp.SetReturn(resultcode.Result_Auth_MakeTokenError)
+		// Get Jwt Info
+		if _, err := auth.GetIAuth().GetJwtInfo(payload.LoginType, payload.Uuid); err != nil {
+			resp.SetReturn(resultcode.Result_Auth_ExpiredJwt)
 		} else {
-			// Delete the uuid in Redis.
-			if err := auth.GetIAuth().DeleteJwtInfo(payload.LoginType, payload.Uuid); err != nil {
-				resp.SetReturn(resultcode.Result_RedisError)
+			// Make Renew Token.
+			if newJwtInfoValue, err := auth.GetIAuth().MakeToken(payload); err != nil {
+				resp.SetReturn(resultcode.Result_Auth_MakeTokenError)
+			} else {
+				// Delete the uuid in Redis.
+				if err := auth.GetIAuth().DeleteUuidRedis(payload.LoginType, payload.Uuid); err != nil {
+					resp.SetReturn(resultcode.Result_RedisError)
+				} else {
+					resp.Value = newJwtInfoValue
+				}
 			}
-			resp.Value = jwtInfoValue
 		}
 	}
 	return c.JSON(http.StatusOK, resp)
