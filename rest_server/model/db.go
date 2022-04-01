0d0cb77c1dfc71d08d6684b2a70bb6d01bfb3ea1
beomjpark <beomjpark@github.com>
2022-04-01 15:26:47 +0900

    :hammer: Fixed DB Connection idleSize

--- rest_server/model/db.go
+++ rest_server/model/db.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"strconv"
+	"time"
 
 	baseconf "github.com/ONBUFF-IP-TOKEN/baseapp/config"
 	"github.com/ONBUFF-IP-TOKEN/basedb"
@@ -67,10 +68,10 @@ func (o *DB) ConnectDB(conf *baseconf.DBAuth) (*basedb.Mssql, error) {
 		return nil, err
 	}
 
-	//idleSize, _ := strconv.ParseInt(conf.IdleSize, 10, 32)
-	//mssqlDB.GetDB().SetMaxOpenConns(int(idleSize))
-	//mssqlDB.GetDB().SetMaxIdleConns(int(idleSize))
-	//mssqlDB.GetDB().SetConnMaxLifetime(5 * time.Minute)
+	idleSize, _ := strconv.ParseInt(conf.IdleSize, 10, 32)
+	mssqlDB.GetDB().SetMaxOpenConns(int(idleSize))
+	mssqlDB.GetDB().SetMaxIdleConns(int(idleSize))
+	mssqlDB.GetDB().SetConnMaxLifetime(5 * time.Minute)
 
 	return mssqlDB, nil
 }
