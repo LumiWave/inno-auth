92a072a19960a0ed217c1e23d4527f6f50419348
beomjpark <beomjpark@github.com>
2022-04-01 15:13:20 +0900

    :hammer: Fixed DB Connection options

--- rest_server/model/db_app_account.go
+++ rest_server/model/db_app_account.go
@@ -68,10 +68,15 @@ func (o *DB) AuthMembers(account *context.Account, payload *context.Payload) (*c
 func (o *DB) VerfiyAccounts(innoUID string) (bool, error) {
 	var returnValue orginMssql.ReturnStatus
 	var isExists bool
-	_, err := o.MssqlAccountRead.GetDB().QueryContext(contextR.Background(), USPAU_Verify_Accounts,
+	rows, err := o.MssqlAccountRead.GetDB().QueryContext(contextR.Background(), USPAU_Verify_Accounts,
 		sql.Named("InnoUID", innoUID),
 		sql.Named("IsExists", sql.Out{Dest: &isExists}),
 		&returnValue)
+
+	if rows != nil {
+		defer rows.Close()
+	}
+
 	if err != nil {
 		log.Errorf("USPAU_Verify_Accounts QueryContext: %v", err)
 		return false, err
